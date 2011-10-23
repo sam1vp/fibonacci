a9b3446a31dd35f21a6a0f603bf0fe9243232c9d
Sam Gilbert <sgilbert@barbariangroup.com>
2011-10-23 02:27:36 -0700

    edge case handling on fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,10 +5,25 @@ describe Fixnum do
     156.closest_fibonacci.should == 144
     99.closest_fibonacci.should == 89
   end
+
+  it "should return itself if it's a member of fibonacci" do 
+    144.closest_fibonacci.should == 144
+  end
+  
+  it "should return 0 or 1 if 0 or 1 (respectively) are sent closest_fibonacci" do 
+    0.closest_fibonacci.should == 0
+    1.closest_fibonacci.should == 1
+  end
+
+  it "should return self if self is negative" do 
+    -5.closest_fibonacci.should == -5
+  end
+
 end
 
 class Fixnum
   def closest_fibonacci(low = 0, high = 1)
+    return self if [0,1].include?(self) or self < 0
     new_number = low + high
     return new_number if new_number + high > self
     closest_fibonacci(high, new_number)
