60b63580e0646c3b1e9c4a3b62e43a8dd828aace
Sam Gilbert <sgilbert@barbariangroup.com>
2011-10-23 21:52:29 -0700

    refactored recursive method from 3=>2 variables

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,15 +1,18 @@
 class Fixnum
   def closest_fibonacci
-    next_fib_number
+    recurse_to_closest_fibonacci
   end
 
   private
 
-  def next_fib_number(low = 0, high = 1)
-    return self if [0,1].include?(self) or self < 0
-    new_number = low + high
-    return new_number if new_number + high > self
-    next_fib_number(high, new_number)
+  def recurse_to_closest_fibonacci(low = 0, high = 1)
+    return self if self <= 0
+    if low + high > self   #next number is going to be too large
+      return high    #all done!
+    else 
+      return recurse_to_closest_fibonacci(high, low + high)    #head further down the stack (http://bit.ly/dAx3tT)
+    end
   end
+
 end
 
